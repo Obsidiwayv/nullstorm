1753fa8dcf919a3444b7ae4df8b2597ca84f8718
Obsidiwayv <pizza5803@gmail.com>
2024-04-23 20:51:20 -0400

    rewrite readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
 import SideBar from './components/SideBar';
 
-import { getVersionBanner } from './utils/versions';
+import { getVersionBanner, isSnapshot } from './utils/versions';
 
 import { Play24Filled, Dismiss24Regular } from '@fluentui/react-icons';
-import { Button } from '@nextui-org/react';
+import { Button, Chip } from '@nextui-org/react';
 import { useEffect, useState } from 'react';
 import { events } from './Global';
+import { appConfigDir } from '@tauri-apps/api/path';
 
 function App() {
+  console.log(appConfigDir())
   const [playButtonState, updatePlayButton] = useState<PlayButtonState>("disabled");
-  const [profile, updateProfile] = useState<ProfileManifest>();
+  const [profile, updateProfile] = useState<ProfileManifest>(Object.assign({}));
   useEffect(() => {
-    events.on("profile", (manifest) => console.log(manifest));
+    events.on("profile", (manifest) => updateProfile(manifest));
 
     return () => events.off("profile", () => { });
   }, []);
+
+  profile.lastVersionId = profile.lastVersionId ?? "1.20.4";
+
   return (
     <div>
-      <SideBar />
-      <div className='home min-h-screen flex flex-col text-white flex-1'>
-        <div className='grid grid-cols-2 pt-6'>
-          <div className='place-self-center text-xl'>
-            <h1>1.20.4</h1>
-            <h1>Fabric</h1>
-          </div>
-          <div className=''>
-            <img src={getVersionBanner()} className='max-w-md mx-auto rounded-xl' />
+      <div className='static home min-h-screen flex flex-col text-white flex-1'>
+        <div className='pt-6 pl-60 flex justify-around'>
+          <SideBar />
+          <div className='absolute top-0 right-0 pr-6 pt-6'>
+            <div>
+              <img src={getVersionBanner()} className='max-w-sm mx-auto rounded-xl' />
+            </div>
+            <div className='place-self-center text-xl text-center pt-6'>
+              <h1>{profile.lastVersionId}</h1>
+              <div className='pb-3'>
+                {isSnapshot(profile.type ?? "")
+                  ? <Chip variant='bordered' color='warning'>snapshot</Chip>
+                  : ""}
+              </div>
+            </div>
           </div>
         </div>
         <div className="absolute bottom-0 left-0 right-0 flex justify-end p-6">
--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -4,6 +4,6 @@ export function getVersionBanner() {
     return banner;
 }
 
-export function isSnapshot(content: string | VersionManifest) {
+export function isSnapshot(content: string | ProfileManifest) {
     return ((typeof content === "object") ? content.type : content).endsWith("-snapshot");
 }
